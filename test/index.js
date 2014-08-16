93ee9b3a7a08afe69dd310644fb87138524d0058
Michael Williams <dinosaur@riseup.net>
2014-08-16 05:54:13 +0000

    port tests from mmckegg/observ-grid

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 var test = require('tape');
 var Observ = require('observ');
 var computed = require('observ/computed');
+var isndarray = require('isndarray');
 var Ndarray = require('ndarray');
 
 var ObservNdarray = require('../');
@@ -97,3 +98,101 @@ test("ObservNdarray emits shape changes", function (t) {
 
   t.end()
 });
+
+test("ObservNdarray functions as grid", function (t) {
+
+  t.plan(12);
+
+  var grid = ObservNdarray([0,1,2,3,4,5], [2,3]);
+  // 0 1 2
+  // 3 4 5
+  t.equal(grid.get(0,1), 1);
+  t.equal(grid.get(1,0), 3);
+  t.equal(grid.get(1,1), 4);
+
+  // set
+  var removeListeners = [
+    grid.data(function (value) {
+      t.same(value._diff, [1, 1, 'A']);
+      t.same(value, [0,'A',2,3,4,5]);
+    }),
+    grid(function(value){
+      t.ok(isndarray(value), 'emits ndarray');
+      t.same(value.data._diff, [1, 1, 'A']);
+      t.same(value.data, [0,'A',2,3,4,5]);
+      t.equal(value.get(0,1), 'A');
+    }),
+  ];
+  grid.set(0,1, 'A');
+  removeListeners.forEach(invoke);
+
+  t.same(grid.index(1,0), 3);
+
+  // direct data update
+  var removeListeners = [
+    grid.data(function (value) {
+      t.same(value, [0,'A',2,'Z',4,5]);
+    }),
+    grid(function (value) {
+      t.equal(value.get(1,0), 'Z');
+    }),
+  ]
+  grid.data.put(3, 'Z');
+  removeListeners.forEach(invoke);
+
+  t.end();
+})
+
+test('nested Observ', function(t){
+  t.plan(10);
+  var inner = Observ(0);
+  var grid = ObservNdarray([inner,1,2,3,4,5], [2,3]);
+
+  // set inner
+  var removeListeners = [
+    inner(function (value) {
+      t.equal(value, 'A');
+    }),
+    grid.data(function (value) {
+      t.same(value._diff, [ 0, 1, 'A' ]);
+      t.same(value, ['A',1,2,3,4,5]);
+    }),
+    grid(function (value) {
+      t.same(value.data._diff, [ 0, 1, 'A' ]);
+      t.equal(value.get(0,0), 'A');
+    }),
+  ];
+  inner.set('A');
+  t.equal(grid.get(0,0), inner);
+  removeListeners.forEach(invoke);
+
+  // override with put
+  var removeListeners = [
+    inner(function(value) {
+      t.ok(false, 'this should not get called');
+    }),
+    grid.data(function (value) {
+      t.same(value._diff, [ 0, 1, 'B' ]);
+      t.same(value, ['B',1,2,3,4,5]);
+    }),
+    grid(function (value) {
+      t.equal(value.get(0,0), 'B');
+    }),
+  ];
+  grid.set(0,0, 'B');
+  t.equal(grid.get(0,0), 'B');
+  removeListeners.forEach(invoke);
+  t.end();
+});
+
+test('set beyond internal data.length', function (t) {
+  var grid = ObservNdarray([0], [2,3]);
+  grid.set(1,2, 'Z');
+  t.equal(grid.get(1,2), 'Z');
+  t.same(grid.data(), [0,,,,,'Z']);
+  t.end();
+});
+
+function invoke(f){
+  return f();
+}
